92c211bca588618401e7b9b24cbb646b60283c45
Andres 'SHK' F. P <andrs.fp@gmail.com>
2022-09-14 17:31:59 +0200

    fix scroll indicator for big displays

--- script/main.js
+++ script/main.js
@@ -1,6 +1,6 @@
 function scrollIndicator() {
     console.log(document.documentElement.scrollTop / (document.documentElement.scrollHeight - window.innerHeight));
-    document.querySelector(".indicator-bar").style.width = `${100 * document.documentElement.scrollTop / (document.documentElement.scrollHeight - window.innerHeight)}%`;
+    document.querySelector(".indicator__bar").style.width = `${100 * document.documentElement.scrollTop / (document.documentElement.scrollHeight - window.innerHeight)}%`;
 }
 
 getWorking = () => {
